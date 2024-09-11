b1fde284fea0551afa268f249459940eba71a84e
umapodesta <142595382+umapodesta@users.noreply.github.com>
2024-09-11 11:16:19 -0300

    Not Found

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {Route, Switch} from 'react-router-dom/cjs/react-router-dom'
-
 import Home from './componentes/Home/Home';
+import NotFound from './componentes/NotFound/NotFound';
 
 function App() {
   return (
     <Switch>
     <Route path="/" exact={true} component={Home}/>
-
+    <Route path="" component={NotFound}/>
     </Switch>
   );
 }
---
+++ src/componentes/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+    <React.Fragment>
+      <h1>404 - Page Not Found</h1>
+    </React.Fragment>
+  );
+  
+export default NotFound;
+  
