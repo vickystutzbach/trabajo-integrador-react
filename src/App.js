34ef7e01fc8caef307f424049b0df4f7c80fb969
umapodesta <142595382+umapodesta@users.noreply.github.com>
2024-09-12 01:30:28 -0300

    algo de search results

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Route, Switch} from 'react-router-dom/cjs/react-router-dom'
 import Home from './componentes/screens/Home/Home';
 import NotFound from './componentes/NotFound/NotFound';
+import SearchResults from './componentes/SearchResults/SearchResults';
 
 function App() {
   return (
     <Switch>
     <Route path="/" exact={true} component={Home}/>
+    <Route path="/search/:query" component={SearchResults} />
     <Route path="" component={NotFound}/>
     </Switch>
   );
