d18d8a41723eded18d2747aa59e9a78983ef01e5
vickystutzbach <mstutzbach@udesa.edu.ar>
2024-09-17 20:44:36 -0300

    saque lista de header, estaba de mas

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,16 +1,45 @@
-import React from "react";
-import ListaDeHeader from "../ListaDeHeader/ListaDeHeader";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import BuscarHeader from "../BuscarHeader/BuscarHeader";
 
+class ListaDeHeader extends Component {
+    constructor() {
+        super();
+        this.state = {
+            mostrarOpciones: false
+        };
+    }
 
-function Header() {
+    alternarVisibilidad = () => {
+        this.setState({ mostrarOpciones: !this.state.mostrarOpciones });
+    };
+    render() {
+        return (
+            <nav>
+                <ul className="main-nav">
+                    <li><Link to="/" className="nav-link">Home</Link></li>
+                    <li><Link to="/favoritos" className="nav-link">Favoritos</Link></li>
+                    
+                    <li className="ver-todo-container">
+                        <p className='ver-todo-button'  onClick={this.alternarVisibilidad}>Ver Todo </p>
+                        <section className={'ver-todo-options ' + (this.state.mostrarOpciones ? "show" : "hide")}>
+                            <ul>
+                                <li><Link to="/ver-todo-series-populares">Series Populares</Link></li>
+                                <li><Link to="/ver-todo-series-mas-valoradas">Series Más Valoradas</Link></li>
+                            </ul>
+                        </section>
+                    </li>
+                </ul>
+                
+                <BuscarHeader />
 
-    return (
-        <ListaDeHeader home="Home" favoritos="Favoritos" vertodo="Ver todo"  />
+                <ul className="logo">
+                    <img src="./img/logo2.png" alt="Logo" className="logo-image" />
+                </ul>
+            </nav>
+        );
+    }
+    }
 
-    
+export default ListaDeHeader;
 
-
-
-    )
-}
-export default Header;  //exportamos el componente para que pueda ser utilizado en otros archivos
