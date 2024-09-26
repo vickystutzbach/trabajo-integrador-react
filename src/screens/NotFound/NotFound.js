8ea11b43c6d03727ffb9b94ed7199ebcd6251860
vickystutzbach <mstutzbach@udesa.edu.ar>
2024-09-26 19:16:08 -0300

    cambie el header y footer del not found que estaba repetido

--- src/screens/NotFound/NotFound.js
+++ src/screens/NotFound/NotFound.js
@@ -1,12 +1,8 @@
 import React from "react";
-import Footer from "../../componentes/Footer/Footer";
-import Header from "../../componentes/Header/Header";
 
 const NotFound = () => (
     <React.Fragment>
-      <Header/>
       <h1>404 - Page Not Found</h1>
-      <Footer/>
     </React.Fragment>
 
   );
